26b2a2b48cc07f57365d5ca4e30204752dd3457d
YugalXD <YugalXD@users.noreply.github.com>
2015-12-12 20:53:47 +0530

    congif

--- config/env/default.js
+++ config/env/default.js
@@ -2,9 +2,9 @@
 
 module.exports = {
   app: {
-    title: 'MEAN.JS',
-    description: 'Full-Stack JavaScript with MongoDB, Express, AngularJS, and Node.js',
-    keywords: 'mongodb, express, angularjs, node.js, mongoose, passport',
+    title: 'Brown Box',
+    description: 'A full HR appliaction',
+    keywords: 'HR, Jobs, CV',
     googleAnalyticsTrackingID: process.env.GOOGLE_ANALYTICS_TRACKING_ID || 'GOOGLE_ANALYTICS_TRACKING_ID'
   },
   port: process.env.PORT || 3000,
@@ -22,10 +22,10 @@ module.exports = {
     secure: false
   },
   // sessionSecret should be changed for security measures and concerns
-  sessionSecret: process.env.SESSION_SECRET || 'MEAN',
+  sessionSecret: process.env.SESSION_SECRET || 'BBSESS',
   // sessionKey is set to the generic sessionId key used by PHP applications
   // for obsecurity reasons
-  sessionKey: 'sessionId',
+  sessionKey: 'BBSESSId',
   sessionCollection: 'sessions',
   logo: 'modules/core/client/img/brand/logo.png',
   favicon: 'modules/core/client/img/brand/favicon.ico',
